e116aac1f0ba269462185a584c5a39e91d80af19
NoOneLevi <zxvitor1@hotmail.com>
2024-10-10 18:47:38 -0300

    fix: change the type of mailDetails and put the ContacctForm interface on file

--- internal/handlers/contact_handler.go
+++ internal/handlers/contact_handler.go
@@ -7,16 +7,29 @@ import (
 	"github.com/LeviVromao/backend_challenge/internal/services"
 )
 
+type ContactForm struct {
+	Name              string `json:"name"`
+	Mail              string `json:"mail"`
+	Comment           string `json:"comment"`
+	RecaptchaResponse string `json:"g-recaptcha-response"`
+}
+
 func ContactFormHandler(w http.ResponseWriter, r *http.Request) {
 	var form ContactForm
 	err := json.NewDecoder(r.Body).Decode(&form)
 	if err != nil {
-		http.Error(w, "Invaalid Request", http.StatusBadRequest)
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
 		return
 	}
 
 	if services.VerifyCaptcha(form.RecaptchaResponse) {
-		services.SendMail(form)
+		mailDetails := services.MailDetails{
+			Recipient: form.Mail,
+			Name:      form.Name,
+			Comment:   form.Comment,
+		}
+
+		services.SendMail(mailDetails)
 		w.WriteHeader(http.StatusCreated)
 	} else {
 		http.Error(w, "Captcha Invalido", http.StatusUnauthorized)
